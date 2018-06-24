4f97d7fc62607ccf5aa3b86d80a64112dc50513f
bulevarOviedo <mincenp@gmail.com>
2018-06-24 22:29:25 +0200

    Add description in code

--- assets/js/arcgis.js
+++ assets/js/arcgis.js
@@ -18,7 +18,7 @@ require([
 ], function (
     SceneView, WebScene,Slide, Expand, LayerList, BasemapGallery, domConstruct, domClass, on, query,dom
      
-     //
+     // Create a new WebScene referencing a WebScene ID from ArcGIS Online
 ) {
     var scene = new WebScene({
         portalItem: { // autocasts as new PortalItem()
@@ -26,7 +26,7 @@ require([
         }
     });
 
-
+    // Reference the WebScene in a SceneView instance.
     var view = new SceneView({
         map: scene,
         container: "viewDiv",
@@ -40,23 +40,24 @@ require([
 
     });
 
-    // LayerList
+    // Create var LayerList
     var layerList = new LayerList({
         container: document.createElement("div"),
         view: view
     });
+    // Create a Expand for the LayerList widget
     var layerListExpand = new Expand({
         expandIconClass: "esri-icon-layer-list",
-        // expandTooltip: "Expand LayerList", // optional, defaults to "Expand" for English locale
         view: view,
         content: layerList.domNode
     });
-
+    
+    // Create var basemapGallery
     var basemapGallery = new BasemapGallery({
         container: document.createElement("div"),
         view: view
     });
-    
+    // Create a Expand for the basemapGallery widget
     var baseMapExpand = new Expand({
         expandIconClass: "esri-icon-basemap", 
         view: view,
@@ -69,73 +70,69 @@ require([
         view: view,
         content: slidesDiv
     });
-
+    // Add widget to the top/bottom right/left corner of the view
     view.ui.add(layerListExpand, "top-left");
     view.ui.add(baseMapExpand, "top-left");
     view.ui.add(slidesExpand, "bottom-left");
     
    
-    
+    //
     slidesExpand.expanded = false;
 
-      /*********************************************************************
-       * Function to create the UI for a slide by creating DOM nodes and
-       * adding them to the slidesDiv container.
-       *********************************************************************/
-      function createSlideUI(slide, placement) {
-        /*********************************************************************
-         * Create a new <div> element which contains all the slide information.
-         * Store a reference to the created DOM node so we can use it to place
-         * other DOM nodes and connect events.
-         *********************************************************************/
-        var slideElement = domConstruct.create("div", {
-          // Assign the ID of the slide to the <span> element
-          id: slide.id,
-          className: "slide"
+      
+    //Function to create the UI for a slide by creating DOM nodes and adding them to the slidesDiv container.
+       
+    function createSlideUI(slide, placement) {
+       
+    //Create a new <div> element which contains all the slide information.
+    //Store a reference to the created DOM node so we can use it to place other DOM nodes and connect events.
+         
+    var slideElement = domConstruct.create("div", {
+    // Assign the ID of the slide to the <span> element
+    id: slide.id,
+        className: "slide"
         });
 
-        /*********************************************************************
-         * Place the newly created DOM node at the beginning of the slidesDiv
-         *********************************************************************/
-        var position = placement ? placement : "last";
-        domConstruct.place(slideElement, "slidesDiv", position);
+        
+    //Place the newly created DOM node at the beginning of the slidesDiv
+         
+    var position = placement ? placement : "last";
+    domConstruct.place(slideElement, "slidesDiv", position);
 
-        /*********************************************************************
-         * Create a <div> element to contain the slide title text
-         *********************************************************************/
-        domConstruct.create("div", {
-          // Place the title of the slide in the <div> element
-          textContent: slide.title.text,
-          className: "title"
+       
+    //Create a <div> element to contain the slide title text
+         
+    domConstruct.create("div", {
+    // Place the title of the slide in the <div> element
+            textContent: slide.title.text,
+            className: "title"
         }, slideElement);
 
-        /*********************************************************************
-         * Create a new <img> element and place it inside the newly created slide
-         * element. This will reference the thumbnail from the slide.
-         *********************************************************************/
-        domConstruct.create("img", {
-          // Set the src URL of the image to the thumbnail URL of the slide
-          src: slide.thumbnail.url,
+        
+    //Create a new <img> element and place it inside the newly created slide element. This will reference the thumbnail from the slide.
+         
+    domConstruct.create("img", {
+    // Set the src URL of the image to the thumbnail URL of the slide
+        src: slide.thumbnail.url,
 
-          // Set the title property of the image to the title of the slide
+    // Set the title property of the image to the title of the slide
           title: slide.title.text
         }, slideElement); // Place the image inside the new <div> element
 
-        /*********************************************************************
-         * Set up a click event handler on the newly created slide. When clicked,
-         * the code defined below will execute.
-         *********************************************************************/
+        
+    //Set up a click event handler on the newly created slide. When clicked, the code defined below will execute.
+        
         on(slideElement, "click", function() {
-          /*******************************************************************
-           * Remove the "active" class from all elements with the .slide class
-           *******************************************************************/
+          
+    //Remove the "active" class from all elements with the .slide class
+           
           query(".slide").forEach(function(node) {
             domClass.remove(node, "active");
           });
 
-          /*******************************************************************
-           * Add the "active" class on the current element being selected
-           *******************************************************************/
+         
+    //Add the "active" class on the current element being selected
+           
           domClass.add(slideElement, "active");
 
           
